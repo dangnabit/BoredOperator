09241fb2c70956b6c5422f3501051704b6a1baa8
ElyseDaly <elysedaly10@gmail.com>
2017-07-15 12:54:29 -0500

    added to CueList

--- app/Components/CueList.js
+++ app/Components/CueList.js
@@ -2,15 +2,42 @@
 var React = require('react');
 var Router = require('react-router')
 
-var Main = React.createClass({
+var CueList = React.createClass({
+  
+  handleClick: function (){
+  	console.log(this)
+  },
+
   render: function(){
 
+  	if (this.props.cues.length < 1) {
+      return(
+        <div>
+            <h4><span><em>You dont have any cues yet</em></span></h4>
+        </div>
+      )
+    } else if (this.props.cues ) {
+      var cues = this.props.cues.map(function(cue, index){
+        return(
+            <div key={index}>
+              <li className="cue-list-item" >
+                <button className="btn btn-warning btn-lg" onClick={this.handleClick.bind(this, cue)}>Cue: {cue.number}
+                  <span><em>{cue.number}</em></span>
+                }
+                </button>
+              </li>
+            </div>
+        )
+      }.bind(this))
+    }
+
     return(
-      <div className="col-md-2" id="cue-col">
+      <div id="cue-col">
         <p>Cues</p>
+        {cues}
       </div>
     )
   }
 });
 
-module.exports = Main;
+module.exports = CueList;
