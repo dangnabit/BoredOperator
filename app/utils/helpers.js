eac7ec3a44f0c3efffb1c8023ff4aa95eb801329
John Gorman <jack.m.gman@gmail.com>
2017-07-15 12:10:23 -0500

    Create helpers for Patch and Cue creation

---
+++ app/utils/helpers.js
@@ -0,0 +1,66 @@
+const axios = require('axios');
+
+module.exports = {
+
+	getFixtureByName: function(fixtureName){
+		return axios.get(`/api/fixture/${fixtureName}`)
+			.then((response) =>{
+				console.log(response)
+				return response;
+			})
+			.catch( (err) =>{
+				console.error(err);
+				throw err;
+			})
+	},
+
+	validatePatchLength: function(startChannel, patchLength){
+			return (startChannel + patchLength > 512);
+	},
+
+	createPatch: function(patchFormJSON){
+		let fixtureName = patchJSON.fixtureName;
+		getFixtureByName(fixtureName)
+			.then( (fixtureDoc) =>{
+				let patch = {
+					fixtureName : fixtureDoc.fixtureName,
+					startingChannel: patchFormJSON.startingChannel
+				}
+				try{
+					validatePatchLength(patchFormJSON.startingChannel, fixtureDoc.channelParameters.length);
+				} catch (e){
+					throw e;
+				}
+				return axios.post(`/api/patch`, patch)
+					.then( (response) =>{
+						console.log(`POST api/patch ${response}`);
+						return response;
+					})
+					.catch ( (err) =>{
+						console.error(err);
+						throw err;
+					});
+			})
+			.catch( (error) =>{
+				console.error(error);
+				throw error;
+			});
+	},
+
+
+	createCue: function(cueForm){
+		console.log(`Create Cue`);
+		return axios.post('/api/cues', cueForm).then( (response) =>{
+			console.log(`Sucessfully created cue`);
+			return response;
+		})
+		.catch( (err) =>{
+			console.error(err);
+			throw err;
+		})
+	}
+
+
+
+
+}
