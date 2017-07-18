8b67bfce10b03991b69fcb9fda66d097bb65f08a
ElyseDaly <elysedaly10@gmail.com>
2017-07-17 20:40:50 -0500

    admin branch created

--- app/Components/Admin.js
+++ app/Components/Admin.js
@@ -5,9 +5,13 @@ const Admin = React.createClass({
 	
 	render: function(){
 		return(
-			<div className="adminTools">
-                <p>Admin Console</p>
-            </div>
+			<div className="container-fluid">
+				<div className="row" id="main-page-row">
+					<div className="adminTools">
+						<p>Admin Console</p>
+					</div>
+				</div>
+			</div>
 		)
 	}
 })
