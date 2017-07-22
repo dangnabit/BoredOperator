a9ae703e69b58e5cc83e563e4fef685839a62d94
ElyseDaly <elysedaly10@gmail.com>
2017-07-22 11:30:20 -0500

    tooltips

--- app/Components/CueList.js
+++ app/Components/CueList.js
@@ -34,17 +34,11 @@ var CueList = React.createClass({
     }
 
     return(
-<<<<<<< HEAD
-      <div className="col-md-2" id="cue-col">
-        <p>Cues   <span className="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-placement="top" data-placement="true" title="This is where your saved cues will show up. Press any of the buttons to recall the cue."></span></p>
-        {cues}
-=======
       <div className="col-md-2">
-        <p>Cues</p>
+        <p>Cues <span className="glyphicon glyphicon-info-sign" data-toggle="tooltip" data-placement="top" data-placement="true" title="This is where your saved cues will show up. Press any of the buttons to recall the cue."></span> </p>
         <div id="cue-col">
           {cues}
         </div>
->>>>>>> 853c32fe7b9baff770e0b57f76ccf90ffde94cd3
       </div>
     )
   }
