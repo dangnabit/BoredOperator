a09af76d512cfa4b79272371a93e32aedb4be582
John Gorman <johngorm@usc.edu>
2017-07-22 11:43:59 -0500

    Linted and removed getFixtureByName function calls that caused errors

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,92 +1,125 @@
-const axios = require('axios');
+var axios = require("axios");
 
 module.exports = {
+  getFixtureByName: function(fixtureName) {
+	"use strict";
+    console.log("In GET FIXTURE function", fixtureName);
+    return axios
+      .get("/api/fixture/" + fixtureName)
+      .then(function(response){
+        console.log(response);
+        return response;
+      })
+      .catch(function(err) {
+        console.error(err);
+        throw err;
+      });
+  },
 
-	getFixtureByName: function(fixtureName){
-		return axios.get(`/api/fixture/${fixtureName}`)
-			.then((response) =>{
-				console.log(response)
-				return response;
-			})
-			.catch( (err) =>{
-				console.error(err);
-				throw err;
-			})
-	},
+  validatePatchLength: function(startChannel, patchLength) {
+	"use strict";
+    return startChannel + patchLength > 512;
+  },
 
-	validatePatchLength: function(startChannel, patchLength){
-			return (startChannel + patchLength > 512);
-	},
+  createPatch: function(patchFormJSON) {
+	"use strict";
+    // var fixtureName = patchFormJSON.fixtureName;
+    // console.log(this.getFixtureByName);
+    // this.getFixtureByName(fixtureName)
+    //   .then(function(fixtureDoc) {
+    //     var patch = {
+    //       fixtureName: fixtureDoc.fixtureName,
+    //       startingChannel: patchFormJSON.startingChannel
+    //     };
+    //     try {
+    //       this.validatePatchLength(
+    //         patchFormJSON.startingChannel,
+    //         fixtureDoc.channelParameters.length
+    //       );
+    //     } catch (e) {
+    //       throw e;
+	//     }
+	    //Need to validate the fixture length
+        return axios
+          .post("/api/patch", patch)
+          .then(function(response) {
+			console.log("POST api/patch" + response);
+            return response;
+          })
+          .catch(function(err){
+            console.error(err);
+            throw err;
+          });
+    //   })
+    //   .catch(function(err) {
+    //     console.error(err);
+    //     throw err;
+    //   });
+  },
 
-	createPatch: function(patchFormJSON){
-		let fixtureName = patchJSON.fixtureName;
-		getFixtureByName(fixtureName)
-			.then( (fixtureDoc) =>{
-				let patch = {
-					fixtureName : fixtureDoc.fixtureName,
-					startingChannel: patchFormJSON.startingChannel
-				}
-				try{
-					validatePatchLength(patchFormJSON.startingChannel, fixtureDoc.channelParameters.length);
-				} catch (e){
-					throw e;
-				}
-				return axios.post(`/api/patch`, patch)
-					.then( (response) =>{
-						console.log(`POST api/patch ${response}`);
-						return response;
-					})
-					.catch ( (err) =>{
-						console.error(err);
-						throw err;
-					});
-			})
-			.catch( (error) =>{
-				console.error(error);
-				throw error;
-			});
-	},
+  createCue: function(cueForm) {
+	"use strict";
+    return axios
+      .post("/api/cues", cueForm)
+      .then(function(response){
+        console.log("Sucessfully created cue");
+        return response;
+      })
+      .catch(function(err) {
+        console.error(err);
+        throw err;
+      });
+  },
 
+  createFixture: function(fixtureForm) {
+	"use strict";
+    // console.log(this.getFixtureByName);
+    // this.getFixtureByName(fixtureForm.fixtureName)
+    //   .then(function(result) {
+	// 	  console.log(result);
+    //     if (!result) {
+          return axios
+            .post("/api/fixtures", fixtureForm)
+            .then(function(results) {
+			  if(!results.data.errmsg){
+                console.log("Successfully created fixture");
+				return results;
+			  }
+			  else{
+				  console.error(results.data.errmsg);
+				  throw results.data.errmsg;
+					
+			  }
+            })
+            .catch(function(err) {
+              console.error(err);
+              throw err;
+            });
+    //     }
+    //   })
+    //   .catch(function(err) {
+    //     console.error(err);
+    //     throw err;
+    //   });
+  },
 
-	createCue: function(cueForm){
-		console.log(`Create Cue`);
-		return axios.post('/api/cues', cueForm).then( (response) =>{
-			console.log(`Sucessfully created cue`);
-			return response;
-		})
-		.catch( (err) =>{
-			console.error(err);
-			throw err;
-		})
-	},
+  getCues: function() {
+	"use strict";
+    return axios.get("/api/cues").then(function(results) {
+      return results;
+    });
+  },
 
-	createFixture: function(fixtureForm){
-		console.log(`Fixture create helper`);
-		return axios.post('/api/fixtures', fixtureForm).then( function(results){
-			console.log(`Successfully created fixture`);
-			return results;
-		})
-		.catch(function(err) {
-			console.error(err);
-			throw err;
-		})
-	},
-
-	getCues: function(){
-		return axios.get('/api/cues').then(function(results){
-        	return results;
-    	});
-	},
-
-	getPatch: function(){
-		return axios.get('/api/patch').then(function(results){
-        	return results;
-    	});
-	},
-	getFixtures: function(){
-		return axios.get('/api/fixtures').then(function(results){
-        	return results;
-    	});
-	}
-
-}
+  getPatch: function() {
+	"use strict";
+    return axios.get("/api/patch").then(function(results) {
+      return results;
+    });
+  },
+  getFixtures: function() {
+	"use strict";
+    return axios.get("/api/fixtures").then(function(results) {
+      return results;
+    });
+  }
+};
